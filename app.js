dc251feff0e685f7865810682d42f1dd0b611a4f
shawil ayoul <aocholayoul9@gmail.com>
2024-02-27 15:02:58 +0100

    working on cart sectition

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const navItem = document.querySelectorAll(".navItem")
-const sliders = document.querySelector(".sliders")
+const navItem = document.querySelectorAll(".navItem");
+const sliders = document.querySelector(".sliders");
+const shoeZise = document.querySelectorAll(".shoe-size");
 
 navItem.forEach((item, index) => {
-    item.addEventListener("click", () => {
-        sliders.style.transform = `translateX(${-100 * index}vw)`;
+  item.addEventListener("click", () => {
+    sliders.style.transform = `translateX(${-100 * index}vw)`;
+  });
+});
+
+shoeZise.forEach((shoe, index) => {
+  shoe.addEventListener("click", () => {
+    shoe.style.background = "black";
+    shoe.style.color = "white";
+    shoe.addEventListener("click", () => {
+      shoe.style.background = "white";
+      shoe.style.color = "black";
     });
+  });
 });
